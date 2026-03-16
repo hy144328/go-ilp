a538feb1d3f2b9d1c396fc2426b0df2786a4fc8d
Hans Yu <hans.yu@outlook.de>
2026-03-16 12:31:34 +0100

    feat: Simple operations.

---
+++ numbers/ops.go
@@ -0,0 +1,19 @@
+package numbers
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+func Abs[T constraints.Integer](x T) T {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
+func Remainder[T constraints.Integer](x T, y T) T {
+	y = Abs(y)
+
+	return (x % y + y) % y
+}
---
+++ numbers/ops_test.go
@@ -0,0 +1,46 @@
+package numbers
+
+import (
+	"testing"
+)
+
+func TestAbs(t *testing.T) {
+	var tests = map[string]struct{
+		got int
+		want int
+	}{
+		"positive": {1, 1},
+		"zero": {0, 0},
+		"negative": {-1, 1},
+	}
+
+	for k, v := range tests {
+		t.Run(k, func(t *testing.T) {
+			res := Abs(v.got)
+			if res != v.want {
+				t.Errorf("Abs(%d) = %d != %d", v.got, v.want, res)
+			}
+		})
+	}
+}
+func TestRemainder(t *testing.T) {
+	var tests = map[string]struct{
+		dividend int
+		divisor int
+		want int
+	}{
+		"positive, positive": {5, 3, 2},
+		"positive, negative": {5, -3, 2},
+		"negative, positive": {-5, 3, 1},
+		"negative, negative": {-5, -3, 1},
+	}
+
+	for k, v := range tests {
+		t.Run(k, func(t *testing.T) {
+			res := Remainder(v.dividend, v.divisor)
+			if res != v.want {
+				t.Errorf("Remainder(%d, %d) = %d != %d", v.dividend, v.divisor, v.want, res)
+			}
+		})
+	}
+}
