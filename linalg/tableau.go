44d86af2ea76421507fa32d6a18dc755d89bf800
Hans Yu <hans.yu@outlook.de>
2026-03-18 22:40:08 +0100

    feat(linalg): First draft of tableau module.

---
+++ linalg/tableau.go
@@ -0,0 +1,66 @@
+package linalg
+
+import (
+	"errors"
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+
+	"github.com/hy144328/go-ilp/numbers"
+)
+
+var (
+	ErrZeroPivot = errors.New("zero pivot")
+)
+
+type Tableau[T constraints.Integer] [][]T
+
+// ScaleRow multiplies a row by a constant factor.
+func (tab Tableau[T]) ScaleRow(idx int, fac T) {
+	row := tab[idx]
+
+	for colCt := range row {
+		row[colCt] *= fac
+	}
+}
+
+// EliminateRow subtracts the multiple of one row from the multiple of another row such that the corresponding entry in the column vanishes.
+func (tab Tableau[T]) EliminateRow(srcIdx int, dstIdx int, colIdx int) error {
+	src := tab[srcIdx]
+	dst := tab[dstIdx]
+
+	facSrc := dst[colIdx]
+	facDst := src[colIdx]
+
+	if facSrc == 0 {
+		return nil
+	} else if facDst == 0 {
+		return fmt.Errorf("%w: tab[%d, %d] = 0.", ErrZeroPivot, srcIdx, colIdx)
+	}
+
+	for colCt, colIt := range src {
+		dst[colCt] *= facDst
+		dst[colCt] -= facSrc * colIt
+	}
+
+	return nil
+}
+
+// DeflateRow divides a row by a constant factor such that all entries are coprime.
+func (tab Tableau[T]) DeflateRow(idx int) {
+	row := tab[idx]
+
+	fac := numbers.GreatestCommonDivisor(row[0], row[1:]...)
+	if fac == 0 {
+		return
+	}
+
+	for colCt := range row {
+		row[colCt] /= fac
+	}
+}
+
+// SwapRows exchanges one row with another row.
+func (tab Tableau[T]) SwapRows(srcIdx int, dstIdx int) {
+	tab[srcIdx], tab[dstIdx] = tab[dstIdx], tab[srcIdx]
+}
---
+++ linalg/tableau_test.go
@@ -0,0 +1,111 @@
+package linalg
+
+import (
+	"testing"
+)
+
+func TestScaleRow(t *testing.T) {
+	tests := map[string]struct {
+		tab Tableau[int]
+		idx int
+		fac int
+		want Tableau[int]
+	}{
+		"base": {[][]int{{1, 2}, {3, 4}}, 0, 2, [][]int{{2, 4}, {3, 4}}},
+	}
+
+	for k, v := range tests {
+		t.Run(k, func(t *testing.T) {
+			v.tab.ScaleRow(v.idx, v.fac)
+
+			if !equalTableaus(v.tab, v.want) {
+				t.Errorf("%v != %v", v.tab, v.want)
+			}
+		})
+	}
+}
+
+func TestEliminateRow(t *testing.T) {
+	tests := map[string]struct {
+		tab Tableau[int]
+		srcIdx int
+		dstIdx int
+		colIdx int
+		want Tableau[int]
+	}{
+		"base": {[][]int{{1, 2}, {3, 4}}, 0, 1, 0, [][]int{{1, 2}, {0, -2}}},
+	}
+
+	for k, v := range tests {
+		t.Run(k, func(t *testing.T) {
+			v.tab.EliminateRow(v.srcIdx, v.dstIdx, v.colIdx)
+
+			if !equalTableaus(v.tab, v.want) {
+				t.Errorf("%v != %v", v.tab, v.want)
+			}
+		})
+	}
+}
+
+func TestDeflateRow(t *testing.T) {
+	tests := map[string]struct {
+		tab Tableau[int]
+		idx int
+		want Tableau[int]
+	}{
+		"base": {[][]int{{2, 4}, {3, 4}}, 0, [][]int{{1, 2}, {3, 4}}},
+	}
+
+	for k, v := range tests {
+		t.Run(k, func(t *testing.T) {
+			v.tab.DeflateRow(v.idx)
+
+			if !equalTableaus(v.tab, v.want) {
+				t.Errorf("%v != %v", v.tab, v.want)
+			}
+		})
+	}
+}
+
+func TestSwapRows(t *testing.T) {
+	tests := map[string]struct {
+		tab Tableau[int]
+		srcIdx int
+		dstIdx int
+		want Tableau[int]
+	}{
+		"base": {[][]int{{1, 2}, {3, 4}}, 0, 1, [][]int{{3, 4}, {1, 2}}},
+	}
+
+	for k, v := range tests {
+		t.Run(k, func(t *testing.T) {
+			v.tab.SwapRows(v.srcIdx, v.dstIdx)
+
+			if !equalTableaus(v.tab, v.want) {
+				t.Errorf("%v != %v", v.tab, v.want)
+			}
+		})
+	}
+}
+
+func equalTableaus(
+	tab Tableau[int],
+	other Tableau[int],
+) bool {
+	if len(tab) != len(other) {
+		return false
+	}
+	if len(tab[0]) != len(other[0]) {
+		return false
+	}
+
+	for rowCt := range tab {
+		for colCt := range tab[0] {
+			if tab[rowCt][colCt] != other[rowCt][colCt] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
