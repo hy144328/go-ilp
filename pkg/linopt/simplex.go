0362f3dddefdc4779a1bbdb90fe82510d0ba33ea
Hans Yu <hans.yu@outlook.de>
2026-05-23 22:12:52 +0200

    feat(linopt): First draft of simplex algorithm.

---
+++ pkg/linopt/simplex.go
@@ -0,0 +1,146 @@
+package linopt
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hy144328/go-ilp/pkg/linalg"
+	"golang.org/x/exp/constraints"
+)
+
+var (
+	ErrNotFeasible = errors.New("not feasible")
+	ErrUnboundedAbove = errors.New("unbounded above")
+)
+
+// RunSimplex runs the simplex algorithm given a basic feasible solution.
+func RunSimplex[T constraints.Signed](
+	lp LinearProgram[T],
+	pivots []int,
+) error {
+	if err := initializeSimplex(lp, pivots); err != nil {
+		return err
+	}
+
+	for {
+		varCt, ok := chooseEntering(lp)
+		if !ok {
+			break
+		}
+		colCt := varCt + 1
+
+		conCt, ok := chooseExiting(lp, varCt)
+		if !ok {
+			return ErrUnboundedAbove
+		}
+		rowCt := conCt + 1
+
+		if err := linalg.EliminateUp(lp.Tab, rowCt, colCt); err != nil {
+			panic(err)
+		}
+		if err := linalg.EliminateDown(lp.Tab, rowCt, colCt); err != nil {
+			panic(err)
+		}
+		pivots[conCt] = varCt
+	}
+
+	return nil
+}
+
+// initializeSimplex initializes the linear program given a basic feasible solution.
+// Returns error if pivots give infeasible solution.
+func initializeSimplex[T constraints.Signed](
+	lp LinearProgram[T],
+	pivots []int,
+) error {
+	bIdx := lp.NoVariables() + 1
+
+	for conCt, varCt := range pivots {
+		rowCt := conCt + 1
+		colCt := varCt + 1
+
+		linalg.PivotColumn(lp.Tab, rowCt, colCt)
+
+		if err := linalg.EliminateDown(lp.Tab, rowCt, colCt); err != nil {
+			return fmt.Errorf("%w: %w", ErrNotFeasible, err)
+		}
+	}
+
+	for conCt, varCt := range pivots {
+		rowCt := conCt + 1
+		colCt := varCt + 1
+
+		if err := linalg.EliminateUp(lp.Tab, rowCt, colCt); err != nil {
+			return fmt.Errorf("%w: %w", ErrNotFeasible, err)
+		}
+
+		if lp.Tab[rowCt][bIdx] < 0 {
+			if err := lp.Tab.ScaleRow(rowCt, -1); err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	return nil
+}
+
+// chooseEntering chooses the entering variable based on Bland's rule.
+// If no variable is found, this means that the linear program is finished.
+func chooseEntering[T constraints.Signed](
+	lp LinearProgram[T],
+) (int, bool) {
+	for varCt, varIt := range lp.Tab[0][1:1+lp.NoVariables()] {
+		if varIt < 0 {
+			return varCt, true
+		}
+	}
+
+	return 0, false
+}
+
+// chooseExiting chooses the exiting variable based on the minimum-ratio criterion.
+// If no variable is found, this means that the linear program is unbounded above.
+func chooseExiting[T constraints.Signed](
+	lp LinearProgram[T],
+	varIdx int,
+) (int, bool) {
+	colCt := varIdx + 1
+
+	var res int
+	var ok bool
+
+	for conCt := range lp.NoConstraints() {
+		rowCt := conCt + 1
+
+		if lp.Tab[rowCt][colCt] <= 0 {
+			continue
+		}
+
+		if !ok {
+			res = conCt
+			ok = true
+			continue
+		}
+
+		if checkMinimumRatio(lp, varIdx, res, conCt) {
+			res = conCt
+			ok = true
+		}
+	}
+
+	return res, ok
+}
+
+// checkMinimumRatio evaluates the minimum-ratio criterion.
+func checkMinimumRatio[T constraints.Signed](
+	lp LinearProgram[T],
+	varIdx int,
+	conIdx int,
+	newIdx int,
+) bool {
+	bIdx := lp.NoVariables() + 1
+	lhs := lp.Tab[newIdx+1][bIdx] * lp.Tab[conIdx+1][varIdx+1]
+	rhs := lp.Tab[conIdx+1][bIdx] * lp.Tab[newIdx+1][varIdx+1]
+
+	return lhs < rhs
+}
---
+++ pkg/linopt/simplex_test.go
@@ -0,0 +1,32 @@
+package linopt
+
+import (
+	"testing"
+
+	"github.com/hy144328/go-ilp/pkg/linalg"
+)
+
+func TestRunSimplex(t *testing.T) {
+	form := CanonicalForm[int]{
+		A: linalg.Matrix[int]{
+			{3, 2, 1},
+			{2, 5, 3},
+		},
+		B: linalg.Vector[int]{10, 15},
+		C: linalg.Vector[int]{2, 3, 4},
+	}
+	lp, err := FromStandardForm(form.ToStandard())
+	if err != nil {
+		panic(err)
+	}
+	pivots := []int{3, 4}
+
+	if err := RunSimplex(lp, pivots); err != nil {
+		panic(err)
+	}
+
+	res := lp.Tab[0][1+lp.NoVariables()] / lp.Tab[0][0]
+	if res != 20 {
+		t.Errorf("Got: %v. Expected: %v.", res, 20)
+	}
+}
